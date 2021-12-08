8723c72210d6f3509e6be7e1875d47397c3a635e
luciano <lucianobr04@gmail.com>
2021-12-08 12:26:15 -0300

    Criando verificações nos dados enviados

--- apiAirport.py
+++ apiAirport.py
@@ -1,15 +1,40 @@
 from flask import Flask
 from flask_restplus import Api, Resource
-
+from flask import Response
+from datetime import datetime
 from main import server
+from request import Request
 
 app, api = server.app, server.api
 
-data = [
-    {'id':0, 'title':'War and Peace'}
-]
+def verifyIata(iataOrigem, iataVolta, dataIda, dataVolta):
+    date1 = datetime.strptime(dataIda, '%Y-%m-%d').date()
+    date2 = datetime.strptime(dataVolta, '%Y-%m-%d').date()
 
-@api.route('/air')
+    if iataOrigem == iataVolta:
+        return False
+    elif date2 <= date1:
+        return False
+
+@api.route('/voo/<iataOrigem>/<iataDestino>/<dataIda>', defaults={"dataVolta":None})
+@api.route('/voo/<iataOrigem>/<iataDestino>/<dataIda>/<dataVolta>')
 class ApiAirport(Resource):
-    def get(self):
-        return data
+    def get(self, iataOrigem, iataDestino, dataIda, dataVolta):
+        origem = str(iataOrigem)
+        destino = str(iataDestino)
+        ida = str(dataIda)
+        volta = str(dataVolta) if dataVolta else None
+
+        verifyData = verifyIata(origem, destino, ida, volta)
+
+        if verifyData == False:
+            return ({'error':'Origem e destino iguais'}, 400)
+
+        data = Request(origem,destino, ida, volta)
+
+        dataVoo = data.requestApi()
+
+        if dataVoo == False:
+            return ({'error': 'iata inexistente'}, 400)
+
+        return dataVoo
