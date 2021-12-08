5700a0d4b2676ae20d1882331c416908c6c40118
luciano <lucianobr04@gmail.com>
2021-12-08 12:25:08 -0300

    Criando opções de  request com data de ida e volta

--- request.py
+++ request.py
@@ -1,13 +1,49 @@
 import requests
 
 class Request():
-    def __init__(self):
+    def __init__(self, iataOrigem, iataDestino, dataIda, dataVolta=None):
         self.apikey = 'qEbvlDxInweeAIjmOzEl9vKKKMrdkvLV'
-        self.url = 'http://stub.2xt.com.br/air/search/{}/POA/MAO/2022-06-12'.format(apikey)
+        self.iataOrigem = iataOrigem
+        self.iataDestino = iataDestino
+        self.dataIda = dataIda
+        self.dataVolta = dataVolta
         self.username = 'test'
         self.senha = 'tB7vlD'
 
+    def buildUrlRequest(self):
+
+        if self.dataVolta == None:
+            url = 'http://stub.2xt.com.br/air/search/{}/{}/{}/{}' \
+                .format(self.apikey, self.iataOrigem, self.iataDestino, self.dataIda)
+            return url
+        else:
+            reqIda = 'http://stub.2xt.com.br/air/search/{}/{}/{}/{}' \
+                .format(self.apikey, self.iataOrigem, self.iataDestino, self.dataIda)
+
+            reqVolta = 'http://stub.2xt.com.br/air/search/{}/{}/{}/{}'\
+                .format(self.apikey, self.iataDestino, self.iataOrigem, self.dataVolta)
+
+            return reqIda, reqVolta
+
+
     def requestApi(self):
-        result = requests.get(url=self.url, auth=(self.username, self.senha))
-        data = result.json()
-        return data
+
+        url = self.buildUrlRequest()
+
+        if self.dataVolta == None:
+            result = requests.get(url, auth=(self.username, self.senha))
+            data = result.json() if result else None
+            return data
+
+        else:
+            req1, req2 = url[0], url[1]
+            result = requests.get(url[0], auth=(self.username, self.senha))
+
+            result2 = requests.get(url[1], auth=(self.username, self.senha))
+
+            data = {'ida':result.json() if result else None, 'volta':result2.json() if result else None}
+
+            if data.get('ida') == None or data.get('volta') == None:
+                data = False
+
+            return data
